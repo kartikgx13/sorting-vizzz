2229bf303d14a0394f010d41059fbddbbf05c644
kartikgx13 <108775715+kartikgx13@users.noreply.github.com>
2023-07-02 15:49:06 +0530

    kartik gavande 6th commit date 02/07/2023

--- src/pages/index.js
+++ src/pages/index.js
@@ -121,6 +121,51 @@ async function startInsertion(){
   enableBtns();
 }
 
+async function selection(){
+  const arrSpeed = document.getElementById('speed-slider');
+  const delay = 101 - arrSpeed.value;
+
+  console.log('In selection()');
+  const ele = document.querySelectorAll(".array-bar");
+  for(let i = 0; i < ele.length; i++){
+      console.log('In ith loop');
+      let min_index = i;
+      // Change color of the position to swap with the next min
+      ele[i].style.background = 'blue';
+      for(let j = i+1; j < ele.length; j++){
+          console.log('In jth loop');
+          // Change color for the current comparision (in consideration for min_index)
+          ele[j].style.background = 'red';
+
+          await waitforme(delay);
+          if(parseInt(ele[j].style.height) < parseInt(ele[min_index].style.height)){
+              console.log('In if condition height comparision');
+              if(min_index !== i){
+                  // new min_index is found so change prev min_index color back to normal
+                  ele[min_index].style.background = '#22A39F';
+              }
+              min_index = j;
+          } 
+          else{
+              // if the currnent comparision is more than min_index change is back to normal
+              ele[j].style.background = '#22A39F';
+          }   
+      }
+      await waitforme(delay);
+      swap(ele[min_index], ele[i]);
+      // change the min element index back to normal as it is swapped 
+      ele[min_index].style.background = '#22A39F';
+      // change the sorted elements color to green
+      ele[i].style.background = 'green';
+  }
+}
+
+async function startSelection(){
+  disableBtns();
+  await selection();
+  enableBtns();
+}
+
 
 export default function Home() {
   const [array, setArray] = useState([]);
@@ -164,14 +209,14 @@ export default function Home() {
             <button id='insertion' disabled={array.length==0 ? true : false} className='sort-btn' onClick={startInsertion}>
             Insertion Sort
             </button>
+            <button id='selection' disabled={array.length==0 ? true : false} className='sort-btn' onClick={startSelection}>
+            Selection Sort
+            </button>
             <button id='quick' disabled={array.length==0 ? true : false} className='sort-btn'>
             Quick Sort
             </button>
             <button id='merge' disabled={array.length==0 ? true : false} className='sort-btn'>
             Merge Sort
-            </button>
-            <button id='selection' disabled={array.length==0 ? true : false} className='sort-btn'>
-            Selection Sort
             </button>
         </div>
 
